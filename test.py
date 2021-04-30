58d2ae0e678bcc85d300f80df53352426708bd64
avec140 <avec140@naver.com>
2021-04-30 18:19:55 +0900

    init

---
+++ test.py
@@ -0,0 +1,17 @@
+radius = 10
+
+area = 3.14 * radius *radius
+
+print("반지름", radius, "인원의 면적=", area)
+
+
+
+a = 1
+b = 2
+sum = a+b
+print("합은", sum)
+
+
+score = 10
+score = score +1
+print("합", score)
